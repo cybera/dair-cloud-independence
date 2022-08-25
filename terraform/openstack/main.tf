34a498b6d662070ce81a547c6ce1228bc4953a9d
SnakesAndLadders <shawn.ayotte@cybera.ca>
2022-08-25 23:27:12 +0000

    changing security group IP

--- terraform/openstack/main.tf
+++ terraform/openstack/main.tf
@@ -37,7 +37,7 @@ resource "openstack_networking_secgroup_rule_v2" "tcp_ssh" {
   protocol         = "tcp"
   port_range_min   = 22
   port_range_max   = 22
-  remote_ip_prefix = "${var.allowed_net}"
+  remote_ip_prefix = "0.0.0.0"
 
   security_group_id = "${openstack_networking_secgroup_v2.django.id}"
 }
