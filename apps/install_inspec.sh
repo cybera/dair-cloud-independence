aeea6ec1004ef2c16e061260b53437314f4cc1a0
Shawn Ayotte <shawn.ayotte@cybera.ca>
2022-09-08 11:42:05 -0600

    Fixing Terratest and improving Inspec

--- apps/install_inspec.sh
+++ apps/install_inspec.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-sudo apt-get update
 sudo apt-get -y install software-properties-common
 sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo apt-get update
@@ -10,3 +9,5 @@ sudo /usr/share/rvm/bin/rvm install ruby 2.7
 # Below line should be safe as long as 2.7/2 is the default 2.7, but is a source of breakage
 sudo ln -s /usr/share/rvm/rubies/ruby-2.7.2/bin/gem /usr/local/bin/gem
 sudo ln -s /usr/share/rvm/rubies/ruby-2.7.2/bin/ruby /usr/local/bin/ruby
+# Remove gemwrappers 
+sudo gem uninstall gem-wrappers
